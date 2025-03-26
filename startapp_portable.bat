25ca122479b4cd8c16a02eb79db4484161de295f
coltonisgod234 <141529267+coltonisgod234@users.noreply.github.com>
2025-03-26 10:00:45 -0400

    Update startapp_portable.bat

--- startapp_portable.bat
+++ startapp_portable.bat
@@ -3,7 +3,7 @@
 TITLE Streamutils (CONSOLE) (MSWIN)
 
 @ECHO ON
-python3 src/ttsfront.py -C res/defaultconfig.ini youtube.com/watch?v=jfKfPfyJRdk
+python src/ttsfront.py -C res/defaultconfig.ini youtube.com/watch?v=jfKfPfyJRdk
 
 @ECHO OFF
 ECHO.
@@ -12,4 +12,7 @@ ECHO The program exited!
 ECHO.
 ECHO The return code is %ERRORLEVEL%
 ECHO zero means success, nonzero means error
+ECHO.
+ECHO Paused to inspect crash log
+ECHO Press any key to close this window
 PAUSE
